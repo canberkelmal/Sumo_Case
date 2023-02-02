395bf98e2cc0249a4c7b935142a08a67252caf42
canberkelmal <elmalcanberk@gmail.com>
2023-02-02 13:20:44 +0300

    JoyStickImported

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -1,11 +1,19 @@
+using Sirenix.OdinInspector;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
     CharacterController playerController;
+    Vector3 moveVector;
+
+    [TabGroup("GamePlay")]
+    public float playerSpeed;
+    [TabGroup("UI")]
+    public Joystick joystick;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +23,28 @@ public class PlayerScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        InputController();
+        InputsController();
     }
 
-    void InputController()
+    void InputsController()
     {
-        throw new NotImplementedException();
+
+
+
+        moveVector = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        playerController.Move(moveVector * playerSpeed * Time.deltaTime);
+
+
+
+        //Restart the scene when "R" key is pressed.(to use during developing and testing)
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Restart();
+        }
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        //Time.timeScale = 1;
     }
 }
