acfc885c5103dc5ef77cb224115575a5a55fcdae
canberkelmal <elmalcanberk@gmail.com>
2023-02-02 16:27:32 +0300

    TurningMechanicDone

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -11,7 +11,7 @@ public class PlayerScript : MonoBehaviour
     CharacterController playerController;
     Vector3 moveVector = Vector3.zero;
     Vector3 moveVectorX, moveVectorZ, camOfs;
-    GameObject cam, controlTarget;
+    GameObject cam, controlTarget, rotationController;
     NavMeshAgent playerAgent;
 
     [TabGroup("GamePlay")]
@@ -19,12 +19,15 @@ public class PlayerScript : MonoBehaviour
     [TabGroup("UI")]
     public Joystick joystick;
     [TabGroup("GameData")]
+    public float playerRotateSpeed;
+    [TabGroup("GameData")]
     public float cameraFallowSensivity;
 
     void Start()
     {
         cam = GameObject.Find("Main Camera");
         controlTarget = GameObject.Find("Control Target");
+        rotationController = GameObject.Find("RotationController");
         camOfs = transform.position - cam.transform.position;
     }
 
@@ -41,8 +44,10 @@ public class PlayerScript : MonoBehaviour
             moveVector = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
         }
         controlTarget.transform.position = transform.position + moveVector;
-        transform.LookAt(controlTarget.transform);
-        transform.position += transform.forward * Time.deltaTime * playerSpeed;
+        rotationController.transform.position = transform.position;
+        rotationController.transform.LookAt(controlTarget.transform);
+        transform.rotation = Quaternion.RotateTowards(transform.rotation, rotationController.transform.rotation, playerRotateSpeed * Time.deltaTime) ; 
+        transform.position = Vector3.Lerp(transform.position, transform.position + transform.forward * 5, playerSpeed * Time.deltaTime);
         //playerAgent.destination = controlTarget.transform.position;
 
         //playerController.Move(moveVector * playerSpeed * Time.deltaTime);
