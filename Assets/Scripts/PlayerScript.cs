836d4df69ff4395c830896e278fb4d580e645065
canberkelmal <elmalcanberk@gmail.com>
2023-02-02 15:35:03 +0300

    MovementMechanicDone

--- Assets/Scripts/PlayerScript.cs
+++ Assets/Scripts/PlayerScript.cs
@@ -3,44 +3,61 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 using UnityEngine.SceneManagement;
 
 public class PlayerScript : MonoBehaviour
 {
     CharacterController playerController;
-    Vector3 moveVector;
+    Vector3 moveVector = Vector3.zero;
+    Vector3 moveVectorX, moveVectorZ, camOfs;
+    GameObject cam, controlTarget;
+    NavMeshAgent playerAgent;
 
     [TabGroup("GamePlay")]
     public float playerSpeed;
     [TabGroup("UI")]
     public Joystick joystick;
-    // Start is called before the first frame update
+    [TabGroup("GameData")]
+    public float cameraFallowSensivity;
+
     void Start()
     {
-        playerController= GetComponent<CharacterController>();
+        cam = GameObject.Find("Main Camera");
+        controlTarget = GameObject.Find("Control Target");
+        camOfs = transform.position - cam.transform.position;
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
         InputsController();
+        CamController();
     }
 
     void InputsController()
     {
+        if(joystick.Vertical != 0 || joystick.Horizontal != 0)
+        {
+            moveVector = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
+        }
+        controlTarget.transform.position = transform.position + moveVector;
+        transform.LookAt(controlTarget.transform);
+        transform.position += transform.forward * Time.deltaTime * playerSpeed;
+        //playerAgent.destination = controlTarget.transform.position;
 
-
-
-        moveVector = new Vector3(joystick.Horizontal, 0, joystick.Vertical);
-        playerController.Move(moveVector * playerSpeed * Time.deltaTime);
-
-
+        //playerController.Move(moveVector * playerSpeed * Time.deltaTime);
 
         //Restart the scene when "R" key is pressed.(to use during developing and testing)
         if (Input.GetKeyDown(KeyCode.R))
         {
             Restart();
         }
+    }
+    
+    void CamController()
+    {
+        //cam.transform.position = Vector3.Lerp(cam.transform.position, transform.position - camOfs, cameraFallowSensivity * Time.deltaTime);
+        cam.transform.position = transform.position - camOfs;
     }
     public void Restart()
     {
